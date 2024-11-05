3ad9490d64b9a96c1ed4227fef03db2034ed6924
Vlada Shevchenko <vlada.shevchenko.it@gmail.com>
2024-11-05 02:04:26 +0200

    Lesson 1 (Ruby): Homework Fix

--- ruby/lesson-01/index.rb
+++ ruby/lesson-01/index.rb
@@ -1,38 +1,40 @@
+# frozen_string_literal: true
+
 require_relative('student')
 
-student = Student.new('Sh', 'Vlada', '12/12/2000')
+student = Student.add_student('Sh', 'Vlada', '28/10/2000')
 
-puts "Surname: #{student.surname}"
-puts "Name: #{student.name}"
-puts "Date of birth: #{student.date_of_birth}"
-puts "Age: #{student.calculate_age}"
+puts 'Surname: #{student.surname}'
+puts 'Name: #{student.name}'
+puts 'Date of birth: #{student.date_of_birth}'
+puts 'Age: #{student.calculate_age}'
 
-puts "Student with the age = 24: #{ Student.get_students_by_age(234).map { 
+puts "Student with the age = 24: #{ Student.get_students_by_age(24).map { 
     |student| [
-        "Surname: #{student.surname},", 
-        "Name: #{student.name},", 
-        "Date of birth: #{student.date_of_birth},", 
-        "Age: #{student.calculate_age}", 
+        'Surname: #{student.surname},', 
+        'Name: #{student.name},', 
+        'Date of birth: #{student.date_of_birth},', 
+        'Age: #{student.calculate_age}', 
     ].join(' ') }}"
-puts "Student with the name = Vlada: #{ Student.get_students_by_name("Vlartrtda").map { 
+puts "Student with the name = Vlada: #{ Student.get_students_by_name('Vlada').map { 
     |student| [
-        "Surname: #{student.surname},", 
-        "Name: #{student.name},", 
-        "Date of birth: #{student.date_of_birth},", 
-        "Age: #{student.calculate_age}", 
+        'Surname: #{student.surname},', 
+        'Name: #{student.name},', 
+        'Date of birth: #{student.date_of_birth},', 
+        'Age: #{student.calculate_age}', 
     ].join(' ') }}"
 
-student2 = Student.new('Sh2', 'Vlada2', '12/12/2002')
-student3 = Student.new('Sh3', 'Vlada3', '12/12/2003')
+student2 = Student.add_student('Sh2', 'Vlada2', '12/12/2002')
+student3 = Student.add_student('Sh3', 'Vlada3', '12/12/2003')
 
 p Student.get_all_students
 
-student.remove_student
+Student.remove_student(student)
 p Student.get_all_students
 
 # add student with the same info
-student4 = Student.new('Sh2', 'Vlada2', '12/12/2002')
+student4 = Student.add_student('Sh2', 'Vlada2', '12/12/2002')
 p Student.get_all_students
 
 # add student with bad date of birth
-student4 = Student.new('Sh4', 'Vlada4', '31/10/2024')
+student4 = Student.add_student('Sh4', 'Vlada4', '31/10/2024')
--- ruby/lesson-01/student.rb
+++ ruby/lesson-01/student.rb
@@ -1,42 +1,60 @@
+# frozen_string_literal: true
+
 require 'date'
 
 class Student
     @@students = []
-    attr_accessor :surname, :name, :date_of_birth
+    attr_accessor :name, :surname, :date_of_birth
 
     def initialize(surname, name, date_of_birth)
+        self.date_of_birth = date_of_birth
         @surname = surname
         @name = name
-        self.date_of_birth = date_of_birth
-        add_student
     end
 
     def date_of_birth=(date_of_birth)
-        
         date_of_birth = Date.parse(date_of_birth)
         if date_of_birth < Date.today
             @date_of_birth = date_of_birth
         else
-            raise ArgumentError, "Invalid date of birth"
+            raise ArgumentError, 'Invalid date of birth'
         end
     end
 
     def calculate_age
         today = Date.today
-        age = today.year - date_of_birth.year
-        age -= 1 if today.month == date_of_birth.month && today.day < date_of_birth.day
+        age = today.year - @date_of_birth.year
+        age -= 1 if (
+            today.month < @date_of_birth.month || 
+            (today.month == @date_of_birth.month && today.day < @date_of_birth.day)
+            )
         age
     end
 
-    def add_student
-        @@students << self unless @@students.any? { |student| 
-        student.name == self.name && 
-        student.surname == self.surname && 
-        student.date_of_birth == self.date_of_birth }
+    def self.add_student(surname, name, date_of_birth)
+        if @@students.any? { |student| 
+            student.surname == surname &&
+            student.name == name &&
+            student.date_of_birth == Date.parse(date_of_birth) 
+        }
+            raise ArgumentError, 'This student is already in list'
+        end
+
+        student = Student.new(surname, name, date_of_birth)
+        @@students << student
+        student
     end
 
-    def remove_student
-        @@students.delete(self)
+    def self.remove_student(student)
+        if student.is_a?(Student)
+            @@students.delete_if do |s| 
+                s.name == student.name && 
+                s.surname == student.surname && 
+                s.date_of_birth == student.date_of_birth
+            end
+        else
+            false
+        end
     end
 
     def self.get_students_by_age(age)
@@ -49,5 +67,9 @@ class Student
 
     def self.get_all_students()
         @@students
+    end
+
+    def self.clean_students()
+        @@students = []
     end
 end
